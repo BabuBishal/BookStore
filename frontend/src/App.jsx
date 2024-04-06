9eb6becce8973f0c83dcc9f6f04a1e33764005a5
Bishal <bohorabishal20@gmail.com>
2024-04-06 15:10:29 +0545

    delete book

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import CreateBooks from './pages/CreateBooks'
 import EditBook from './pages/EditBook'
-import DeleteBooks from './pages/DeleteBooks'
+import DeleteBook from './pages/DeleteBook'
 import ShowBooks from './pages/ShowBooks'
 
 const App = () => {
@@ -13,7 +13,7 @@ const App = () => {
       <Route path='/' element={<Home />} />
       <Route path='/books/create' element={<CreateBooks />} />
       <Route path='/books/edit/:id' element={<EditBook />} />
-      <Route path='/books/delete/:id' element={<DeleteBooks />} />
+      <Route path='/books/delete/:id' element={<DeleteBook />} />
       <Route path='/books/details/:id' element={<ShowBooks />} />
     </Routes>
   )
